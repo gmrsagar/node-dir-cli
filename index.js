4702731efd70bd07346c3979a0b1f82e4d319af9
Sagar Ghimire <gmrsagar@gmail.com>
2019-04-16 14:28:46 +1000

    Add functions * Traverse given directory * Check if file or folder

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 'use strict'
 const path = require('path')
 const fs = require('fs')
-const sys = []
-fs.readdirSync(__dirname).forEach(file => {
-	sys.push(file)
-})
+let sys = {}
 
-console.log(sys)
+const dirTree = (filename) => {
+	let obj = {}
+	let child = ''
+	let stats = fs.statSync(filename)
+	if (stats.isDirectory()) {
+		obj = {type: 'dir', name: path.basename(filename)}
+		fs.readdirSync(filename).forEach(file => {
+			child = dirTree(filename + '/' + file)
+		})
+
+		obj.children = []
+		obj['children'].push(child)
+	} else {
+		obj = {type: 'file', name: path.basename(filename)}
+	}
+	return obj
+}
+
+const getDir = (dirname) => {
+	sys.type = 'dir'
+	sys.name = path.basename(dirname)
+	sys.children = []
+	fs.readdirSync(dirname).filter(file => {return file !== '.git'}).forEach(file => {
+		let obj = dirTree(file)
+		sys['children'].push(obj)
+	})
+	return sys
+}
+// console.log(sys)
+console.log(JSON.stringify(getDir(__dirname)))
